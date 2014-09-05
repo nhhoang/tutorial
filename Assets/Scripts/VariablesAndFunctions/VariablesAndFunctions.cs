65907f7d0b7ced1b525ea06ce46360ea756bef4d
tit1995 <tit1995@outlook.com>
2014-09-06 00:03:37 +0700

    add new files

---
+++ Assets/Scripts/VariablesAndFunctions/VariablesAndFunctions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour {
+	int a = 9;
+
+	// Use this for initialization
+	void Start () {
+		a = NhanDoi(a) ;
+		Debug.Log (a) ;
+	}
+
+	int NhanDoi (int So)
+	{
+		int kq ;
+    kq = So * 2;
+    return kq;
+	}
+
+	
+	
+}
