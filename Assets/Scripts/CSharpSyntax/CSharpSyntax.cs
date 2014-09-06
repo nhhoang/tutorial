e0609755b63ffd317954d5c8f52db6975a1407b9
tit1995 <tit1995@outlook.com>
2014-09-07 01:12:41 +0700

    add new files

--- Assets/Scripts/CSharpSyntax/CSharpSyntax.cs
+++ Assets/Scripts/CSharpSyntax/CSharpSyntax.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class CSharpSyntax : MonoBehaviour {
 	public int value = 5;
-	int MyFunction (int InputNumber) {
-        int result = value * InputNumber;
-        return result;
-    }
+
+	public int MyFunction(int InputNumber) {
+    int result = value * InputNumber;
+    return result;
+  }
 }
--- Assets/Scripts/ConventionsandSyntax/ConventionsandSyntax.cs
+++ Assets/Scripts/ConventionsandSyntax/ConventionsandSyntax.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class ConventionsandSyntax : MonoBehaviour {
-
 	void Start() {
 		Debug.Log(transform.position.x);
 		if(transform.position.y <= 5f) {
